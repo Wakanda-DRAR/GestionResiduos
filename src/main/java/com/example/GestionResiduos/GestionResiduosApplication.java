edb11d3ea93a75ba530fdac35e0fef6835dff0e1
dxn1l <danielmorenorey2012@gmail.com>
2024-12-06 19:11:41 +0100

    Added README

---
+++ src/main/java/com/example/GestionResiduos/GestionResiduosApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionResiduos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionResiduosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionResiduosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionResiduos/GestionResiduosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionResiduos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionResiduosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
