4bf3dfe7fdd2b4e90b4802202c08060febcf188b
Rodrivdlc <rodri.vdlc@gmail.com>
2024-12-09 01:11:41 +0100

    fixes

--- src/main/java/com/example/GestionResiduos/GestionResiduosApplication.java
+++ src/main/java/com/example/GestionResiduos/GestionResiduosApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionResiduosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GestionResiduosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GestionResiduosApplication.class, args);
+    }
 }
